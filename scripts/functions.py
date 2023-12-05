e271416a612f03ada585960f50dbba482f306d1d
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-12-05 15:24:53 +0330

    Update functions.py
    define create connection method to create any number of custom users.

--- scripts/functions.py
+++ scripts/functions.py
@@ -1,1 +1,18 @@
 import requests
+from scripts.model import UserManager, UserManagerError
+
+user_obj = UserManager()
+
+""" A function to create a new user """
+
+
+def create_connection(data, start, end):
+    for number in range(start, end):
+        try:
+            user_obj.creat(number, data)
+            print("Success")
+        except UserManagerError as e:
+            print(f"Failed: {e}")
+
+
+""" A function to get and save the user subscription link """

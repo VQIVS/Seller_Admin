50809a6c2f9dbbb447ee5dce68e6ff410dd07545
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-12-05 15:19:46 +0330

    Update model.py
    create UserManager class and maintained getting token and init functions.

--- scripts/model.py
+++ scripts/model.py
@@ -5,3 +5,25 @@ import datetime
 
 class UserManagerError(Exception):
     pass
+
+
+"""" A class to handle the User management """
+class UserManager:
+    def __init__(self):
+        self.base_url = 'https://topnet.justadmins.xyz/api/'
+        self.headers = {"Content-Type": "application/json"}
+
+    def get_token(self):
+        url = self.base_url + 'admin/token'
+        data = {
+            "grant_type": "password",
+            "username": "TopNet",
+            "password": "Qi@s1383.83",
+        }
+        response = requests.post(url, data=data)
+        if response.status_code == 200:
+            token = response.json()["access_token"]
+            self.headers["Authorization"] = f"Bearer {token}"
+        else:
+            error_message = f'Error getting access token : {response.status_code}\nresponse body: {response.text}'
+            raise UserManagerError(error_message)

e034fe4a8a61cf1f2de40c72ed349fb7a4ea0aea
AmirMahdi Qiasvand <amirmahdiqiasvand@gmail.com>
2023-12-05 15:20:53 +0330

    Update model.py
    create & maintained method to handle user creation.

--- scripts/model.py
+++ scripts/model.py
@@ -27,3 +27,36 @@ class UserManager:
         else:
             error_message = f'Error getting access token : {response.status_code}\nresponse body: {response.text}'
             raise UserManagerError(error_message)
+
+    def creat(self, num, data_limit):
+        url = self.base_url + 'user'
+        now = datetime.datetime.now()
+        expire = now + datetime.timedelta(days=30)
+        expire_seconds = int(expire.timestamp())
+        data = {
+            "username": f"TN_{num}",
+            "proxies": {
+                "vmess": {},
+                "vless": {}
+            },
+            "inbounds": {
+                "vmess": [
+                    "VMess TCP",
+                    "VMess Websocket"
+                ],
+                "vless": [
+                    "VLESS TCP REALITY 8443",
+                    "VLESS TCP REALITY 2056"
+                ]
+            },
+            "expire": expire_seconds,
+            "data_limit": data_limit,
+            "data_limit_reset_strategy": "no_reset",
+            "status": "active",
+            "note": "This user is for TopNet VPN provider.",
+        }
+        response = requests.post(url, json=data, headers=self.headers)
+        if response.status_code != 200:
+            error_message = f'Error creating user: {response.status_code}\nresponse body: {response.text}'
+            raise UserManagerError(error_message)
+
